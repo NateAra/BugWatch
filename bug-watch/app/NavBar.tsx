e4b9185a2f1cb50ecc57d6c8980447f48aeaac4e
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-04 16:11:37 +0200

    refactor: make navigation bar labels dynamic

--- bug-watch/app/NavBar.tsx
+++ bug-watch/app/NavBar.tsx
@@ -3,18 +3,25 @@ import React from "react";
 import { GiAlienBug } from "react-icons/gi";
 
 const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "" },
+  ];
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href={"/"}>
         <GiAlienBug />
       </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href={"/"}>Dashboard</Link>
-        </li>
-        <li>
-          <Link href={""}>Issues</Link>
-        </li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
