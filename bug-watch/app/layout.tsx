356f19e40104d2cffa4b19dec55f6769110b25fa
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-05 12:20:56 +0200

    style: change font variable

--- bug-watch/app/layout.tsx
+++ bug-watch/app/layout.tsx
@@ -3,9 +3,12 @@ import NavBar from "./NavBar";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,8 +22,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme appearance="dark" accentColor="amber" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
