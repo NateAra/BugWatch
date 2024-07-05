0beae65043c87e5491cff209d81ff0dcd635f889
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-05 17:41:30 +0200

    refactor: extract the error message component

---
+++ bug-watch/app/components/ErrorMessage.tsx
@@ -0,0 +1,24 @@
+import { Callout } from "@radix-ui/themes";
+import React, { ReactNode } from "react";
+import { MdNearbyError } from "react-icons/md";
+
+interface ErrorMessageProps {
+  children: ReactNode;
+}
+
+const ErrorMessage = ({ children }: ErrorMessageProps) => {
+  if (!children) {
+    return null;
+  }
+  
+  return (
+    <Callout.Root color="red" className="mb-5">
+      <Callout.Icon>
+        <MdNearbyError />
+      </Callout.Icon>
+      <Callout.Text>{children}</Callout.Text>
+    </Callout.Root>
+  );
+};
+
+export default ErrorMessage;
