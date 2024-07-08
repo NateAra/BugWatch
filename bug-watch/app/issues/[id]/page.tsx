3e910343dd14dcdba9ac112af63ee6161212939d
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-08 13:03:51 +0200

    style: add mardkdown preview

--- bug-watch/app/issues/[id]/page.tsx
+++ bug-watch/app/issues/[id]/page.tsx
@@ -2,7 +2,7 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Heading, Text, Flex, Card } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React from "react";
+import ReactMarkdown from "react-markdown";
 
 interface IssueDetailProps {
   params: {
@@ -26,8 +26,8 @@ const IssueDetailPage = async ({ params }: IssueDetailProps) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className="prose" mt={"4"}>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
