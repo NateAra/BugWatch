1eba304289d9d0fa9a9979203a001d835150bc97
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-09 08:42:43 +0200

    refactor: import statements & format code

--- bug-watch/app/components/Link.tsx
+++ bug-watch/app/components/Link.tsx
@@ -1,17 +1,17 @@
-import NextLink from "next/link"
-import { Link as RadixLink } from "@radix-ui/themes"
+import { Link as RadixLink } from "@radix-ui/themes";
+import NextLink from "next/link";
 
 interface LinkProps {
   href: string;
   children: string;
 }
 
-const Link = ({href, children}: LinkProps) => {
+const Link = ({ href, children }: LinkProps) => {
   return (
     <NextLink href={href} passHref legacyBehavior>
       <RadixLink>{children}</RadixLink>
     </NextLink>
-  )
-}
+  );
+};
 
-export default Link
+export default Link;
