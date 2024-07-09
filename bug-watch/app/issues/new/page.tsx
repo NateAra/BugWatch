25c26e157bf8970b381748d687590777788b4de1
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-09 10:37:41 +0200

    refactor: add issue form

--- bug-watch/app/issues/new/page.tsx
+++ bug-watch/app/issues/new/page.tsx
@@ -1,82 +1,9 @@
-"use client";
-import { ErrorMessage, Spinner } from "@/app/components/index";
-import { issuesSchema } from "@/app/validationSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, TextField } from "@radix-ui/themes";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
-
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import IssueForm from "../_components/IssueForm";
 
 const NewIssuePage = () => {
-  const router = useRouter();
-
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IssueForm>({
-    resolver: zodResolver(issuesSchema),
-  });
-
-  const [error, setError] = useState("");
-
-  const [isSubmitting, setSubmitting] = useState(false);
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      setSubmitting(true);
-      await axios.post("/api/issues", data);
-      router.push("/issues");
-    } catch (error) {
-      setSubmitting(false);
-      setError("An unexpected error occurred");
-    }
-  });
 
   return (
-    <div className="max-w-xl">
-      {/* Server-side Validation */}
-      {error && <ErrorMessage>{error}</ErrorMessage>}
-
-      <form className="space-y-3" onSubmit={onSubmit}>
-        <TextField.Root
-          placeholder="Title"
-          {...register("title")}
-        ></TextField.Root>
-
-        {/* Client-side Validation */}
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
-          )}
-        />
-
-        {/* Client-side Validation */}
-        <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
-        <Button disabled={isSubmitting}>
-          Submit Issue
-          {isSubmitting && <Spinner />}
-        </Button>
-      </form>
-    </div>
+    <IssueForm />
   );
 };
 
